300d0393ee2ab969da6a1e644348e35274274c64
RAFIFEFZET <mrafiffawwaz040706@gmail.com>
2024-11-13 12:19:39 +0700

    adding speed insight

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 // app/page.tsx
 import MusicPlayer from "./components/MusicPlayer";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function Home() {
   return (
     <div>
       <MusicPlayer />
+      <SpeedInsights />
     </div>
   );
 }
